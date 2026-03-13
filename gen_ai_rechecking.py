ab3878974bba76c398e6c86892ae027943d52010
uma malla <llmpinnacle@gmail.com>
2026-03-13 08:52:28 -0400

    added docker file

--- gen_ai_rechecking.py
+++ gen_ai_rechecking.py
@@ -39,7 +39,7 @@ def validate_and_clean(pages: list) -> dict:
     try:
         completion = client.beta.chat.completions.parse(
             temperature=0,
-            model="gemma2:2b",
+            model="tinyllama:latest",
             messages=[
                 {"role": "user", "content": """
     You are a resume parsing validator.
